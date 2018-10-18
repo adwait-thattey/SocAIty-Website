67912df8bf660bfdd744102e7c4a61cb25947903
ANKUR GUPTA <ankur.g17@iiits.in>
2018-10-19 03:11:07 +0530

    add blog detail view and blog edit view

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,get_object_or_404,redirect
 from django.urls import reverse
 from . models import Blog
 from .forms import BlogCreateForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 def blog_list(request):
@@ -22,9 +23,26 @@ def blog_create(request):
         form = BlogCreateForm()
     return render(request,'blog/blog_create.html',{'form':form})
 
-def blog_detail(request,username,blog_slug):
-    blog = get_object_or_404(Blog,username=username,slug=blog_slug)
+def blog_detail(request,username,slug):
+    author = User.objects.get(username=username)
+    blog = get_object_or_404(Blog,author=author,slug=slug)
     context = {
         'blog':blog,
     }
     return render(request,'blog/blog_detail.html',context)
+
+@login_required
+def blog_edit(request,username,slug):
+    author = User.objects.get(username=username)
+    blog = get_object_or_404(Blog,author=author,slug=slug)
+    if request.method == 'POST':
+        form = BlogCreateForm(data=request.POST,instance=blog)
+        if form.is_valid:
+            form.save()
+            return HttpResponseRedirect(reverse('blog:blog_list'))
+    else:
+        form = BlogCreateForm(instance=blog)
+    context = {
+        'form':form,
+    }
+    return render(request,'blog/blog_edit.html',context)
